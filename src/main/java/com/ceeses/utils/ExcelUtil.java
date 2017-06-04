eb186ee1c1ce11004077e8eb410b06122eeb02c2
zhaoshan <15572760238@163.com>
2017-06-04 22:00:48 +0800

    完善初始化

--- src/main/java/com/ceeses/utils/ExcelUtil.java
+++ src/main/java/com/ceeses/utils/ExcelUtil.java
@@ -3,6 +3,7 @@ package com.ceeses.utils;
 /**
  * Created by zhaoshan on 2017/5/30.
  */
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -37,50 +38,50 @@ public class ExcelUtil {
     //默认单元格内容为数字时格式
     private static DecimalFormat df = new DecimalFormat("0");
     // 默认单元格格式化日期字符串
-    private static SimpleDateFormat sdf = new SimpleDateFormat(  "yyyy-MM-dd HH:mm:ss");
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     // 格式化数字
     private static DecimalFormat nf = new DecimalFormat("0.00");
 
     @Autowired
     LnfsdxqDao lnfsdxqDao;
 
-    public List<Lnfsdxq> getLiNianFenShuDuan(File file, Integer sheedIdex){
+    public List<Lnfsdxq> initLnfsdxq(String filePath) {
         List<Lnfsdxq> result = new ArrayList<>();
-        try{
-            file = new File("/Users/zhaoshan/Downloads/lyfsd.xlsx");
+        try {
+            File file = new File(filePath);
             XSSFWorkbook wb = new XSSFWorkbook(file);
             int sheetNum = wb.getNumberOfSheets();
-            for (int i =0 ;i < sheetNum ;i ++ ){
+            for (int i = 0; i < sheetNum; i++) {
                 XSSFSheet sheet = wb.getSheetAt(i);
 
                 Iterator<Row> rowIterator = sheet.rowIterator();
-                while (rowIterator.hasNext()){
+                while (rowIterator.hasNext()) {
                     Row row = rowIterator.next();
 
-                    if (row.getCell(0).getCellTypeEnum().equals(CellType.STRING)){
+                    if (row.getCell(0).getCellTypeEnum().equals(CellType.STRING)) {
                         continue;
                     }
 
                     Lnfsdxq lnfsdxq = new Lnfsdxq();
                     lnfsdxq.setYear(Integer.parseInt(sheet.getSheetName().substring(2)));
-                    lnfsdxq.setCategory(sheet.getSheetName().substring(0,2));
-                    lnfsdxq.setTotalGrade((int)row.getCell(0).getNumericCellValue());
-                    lnfsdxq.setStudentCount((int)row.getCell(1).getNumericCellValue());
-                    lnfsdxq.setRanking((int)row.getCell(2).getNumericCellValue());
-                    lnfsdxq.setRuralAreaCount((int)row.getCell(3).getNumericCellValue());
-                    lnfsdxq.setPasturingAreaCount((int)row.getCell(5).getNumericCellValue());
-                    lnfsdxq.setPreReguarCount((int)row.getCell(7).getNumericCellValue());
-                    lnfsdxq.setRuralTenAdd((int)row.getCell(9).getNumericCellValue());
-                    lnfsdxq.setRuralTwenAdd((int)row.getCell(10).getNumericCellValue());
-                    lnfsdxq.setHanTenAdd((int)row.getCell(11).getNumericCellValue());
-                    lnfsdxq.setHanTwenAdd((int)row.getCell(12).getNumericCellValue());
-                    lnfsdxq.setPasturingTenAdd((int)row.getCell(13).getNumericCellValue());
-                    lnfsdxq.setPasturingTwenAdd((int)row.getCell(14).getNumericCellValue());
+                    lnfsdxq.setCategory(sheet.getSheetName().substring(0, 2));
+                    lnfsdxq.setTotalGrade((int) row.getCell(0).getNumericCellValue());
+                    lnfsdxq.setStudentCount((int) row.getCell(1).getNumericCellValue());
+                    lnfsdxq.setRanking((int) row.getCell(2).getNumericCellValue());
+                    lnfsdxq.setRuralAreaCount((int) row.getCell(3).getNumericCellValue());
+                    lnfsdxq.setPasturingAreaCount((int) row.getCell(5).getNumericCellValue());
+                    lnfsdxq.setPreReguarCount((int) row.getCell(7).getNumericCellValue());
+                    lnfsdxq.setRuralTenAdd((int) row.getCell(9).getNumericCellValue());
+                    lnfsdxq.setRuralTwenAdd((int) row.getCell(10).getNumericCellValue());
+                    lnfsdxq.setHanTenAdd((int) row.getCell(11).getNumericCellValue());
+                    lnfsdxq.setHanTwenAdd((int) row.getCell(12).getNumericCellValue());
+                    lnfsdxq.setPasturingTenAdd((int) row.getCell(13).getNumericCellValue());
+                    lnfsdxq.setPasturingTwenAdd((int) row.getCell(14).getNumericCellValue());
                     result.add(lnfsdxq);
                 }
             }
 
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -89,17 +90,8 @@ public class ExcelUtil {
         return result;
     }
 
-    public static boolean isInteger(String value) {
-        try {
-            Integer.parseInt(value);//判断是否为数字
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
 
-
-    public static void readExcel2003(File file){
+    public static void readExcel2003(File file) {
 
 //            ArrayList<ArrayList<Object>> rowList = new ArrayList<ArrayList<Object>>();
 //            ArrayList<Object> colList;
@@ -111,39 +103,36 @@ public class ExcelUtil {
     }
 
 
-
-    public static void writeExcel(ArrayList<ArrayList<Object>> result,String path){
-        if(result == null){
+    public static void writeExcel(ArrayList<ArrayList<Object>> result, String path) {
+        if (result == null) {
             return;
         }
         HSSFWorkbook wb = new HSSFWorkbook();
         HSSFSheet sheet = wb.createSheet("sheet1");
-        for(int i = 0 ;i < result.size() ; i++){
+        for (int i = 0; i < result.size(); i++) {
             HSSFRow row = sheet.createRow(i);
-            if(result.get(i) != null){
-                for(int j = 0; j < result.get(i).size() ; j ++){
+            if (result.get(i) != null) {
+                for (int j = 0; j < result.get(i).size(); j++) {
                     HSSFCell cell = row.createCell(j);
                     cell.setCellValue(result.get(i).get(j).toString());
                 }
             }
         }
         ByteArrayOutputStream os = new ByteArrayOutputStream();
-        try
-        {
+        try {
             wb.write(os);
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         byte[] content = os.toByteArray();
         File file = new File(path);//Excel文件生成后存储的位置。
-        OutputStream fos  = null;
-        try
-        {
+        OutputStream fos = null;
+        try {
             fos = new FileOutputStream(file);
             fos.write(content);
             os.close();
             fos.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -151,22 +140,26 @@ public class ExcelUtil {
     public static DecimalFormat getDf() {
         return df;
     }
+
     public static void setDf(DecimalFormat df) {
         ExcelUtil.df = df;
     }
+
     public static SimpleDateFormat getSdf() {
         return sdf;
     }
+
     public static void setSdf(SimpleDateFormat sdf) {
         ExcelUtil.sdf = sdf;
     }
+
     public static DecimalFormat getNf() {
         return nf;
     }
+
     public static void setNf(DecimalFormat nf) {
         ExcelUtil.nf = nf;
     }
-
 
 
 }
--- src/main/java/com/ceeses/web/controller/LnfsdxqController.java
+++ src/main/java/com/ceeses/web/controller/LnfsdxqController.java
@@ -2,12 +2,12 @@ package com.ceeses.web.controller;
 
 import com.ceeses.dao.LnfsdxqDao;
 import com.ceeses.dao.LnskfsxDao;
-import com.ceeses.extractor.LnyxlqqkExtractor;
 import com.ceeses.model.Lnfsdxq;
 import com.ceeses.model.Lnskfsx;
 import com.ceeses.utils.ExcelUtil;
 import com.ceeses.web.result.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,18 +34,35 @@ public class LnfsdxqController extends BaseController {
 
     @RequestMapping("/test")
     @ResponseBody
-    public JsonResult extract() throws IOException {
-
-        //excelUtil.getLiNianFenShuDuan(null, 0);
+    public JsonResult test() throws IOException {
         Lnfsdxq lnfsdxq = lnfsdxqDao.queryTotalByGrade(2016,350);
-
         Integer g  = lnfsdxqDao.queryCountByGrade(lnfsdxq);
         System.out.println(lnfsdxq);
-        System.out.println(g);
 
         List<Lnskfsx>  lnskfsxes = lnskfsxDao.queryLnskfsx(null);
-
         System.out.println(lnskfsxes);
+
+        return new JsonResult();
+    }
+
+
+    @RequestMapping("/initLnfsdxq")
+    @ResponseBody
+    public JsonResult initLnfsdxq(String fileName) throws IOException {
+
+        String path = this.getClass().getResource("/").getFile();
+
+        //初始化传入的文件
+        if (null != fileName &&  !"".equals(fileName)){
+            String filePath = path + "/" + fileName;
+            excelUtil.initLnfsdxq(filePath);
+        } else {//初始化默认理工和文科历史数据
+            String wkFilePath = path + "/lnfsd_wk.xlsx";
+            excelUtil.initLnfsdxq(wkFilePath);
+
+            String lkFilePath = path + "/lnfsd_lk.xlsx";
+            excelUtil.initLnfsdxq(lkFilePath);
+        }
 
         return new JsonResult();
     }
